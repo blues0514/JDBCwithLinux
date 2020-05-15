27e7b3536bdb1e1cf92159cb531500dafc09935f
LeeJH <blues0514@naver.com>
2020-05-15 13:29:38 +0900

    first

---
+++ src/com/company/Album.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import lombok.Data;
+
+// boiler-plate code
+
+@Data
+public class Album {
+    private int albumId;
+    private String title;
+    private int artistId;
+}
